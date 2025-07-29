2819da715aa4e35b32f4e17eaf133814d6bafb56
voirfilm-tv <82654068+voirfilm-tv@users.noreply.github.com>
2025-07-29 14:03:32 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ import Lobby from './pages/Lobby';
 import { useEffect, useState } from 'react';
 
 function App() {
+  useEffect(() => {
+    const fetchUserCoins = async () => {
+      await useUserStore.getState().fetchCoins();
+    };
+    fetchUserCoins();
+  }, []);
+
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
