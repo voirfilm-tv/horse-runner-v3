9ac180df6f9ff3e053ae6439f19e955be3cc9898
voirfilm-tv <82654068+voirfilm-tv@users.noreply.github.com>
2025-07-29 22:20:12 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,18 +8,18 @@ module.exports = {
   theme: {
     extend: {},
   },
+  safelist: [
+    "aspect-square",
+    "grid-cols-15",
+    "grid-rows-15",
+    "max-w-[600px]",
+    "ring-lime-500",
+    "bg-green-700",
+    "rounded-2xl",
+    "shadow-2xl",
+    "bg-lime-50",
+    "border-green-800",
+    "bg-emerald-200"
+  ],
   plugins: [],
 }
-
-theme: {
-  extend: {},
-},
-safelist: [
-  "aspect-square",
-  "grid-cols-15",
-  "grid-rows-15",
-  "max-w-[600px]",
-  "ring-lime-500",
-  "bg-green-700",
-  "rounded-2xl",
-]
